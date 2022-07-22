51e233243722f7431ea97f5c0f72265d24086993
Michael Morrissey <mikehmorrissey@gmail.com>
2022-07-22 12:10:16 -0700

    no localhost ?

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Hapi = require("@hapi/hapi");
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: process.env.HOST || "localhost",
+    host: process.env.HOST || "0.0.0.0",
   });
 
   server.route({
